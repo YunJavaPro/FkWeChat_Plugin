b2a7344264fd0cc4d917e1ac55cbeb4a0d82f176
Wzindx <2753761311@qq.com>
2026-05-31 02:15:54 +0800

    feat: 增加 隐藏悬浮窗 插件 (#10)

---
+++ main/plugins/隐藏悬浮窗/main.java
@@ -0,0 +1,12 @@
+// 隐藏悬浮窗 for FkWeChat
+// 版本：1.0
+// 作用：隐藏 FkWeChat 快捷工具悬浮窗。
+
+void onLoad() {
+    setFloat("float_ball_size", 0.0f);
+    log("隐藏悬浮窗 1.0 已启用");
+}
+
+void onUnload() {
+    log("隐藏悬浮窗 1.0 已关闭");
+}
